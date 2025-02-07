bd8c7f4240b0f37b9f025596943b510b54c59532
Vaishnavi <pangarevaishnavi639@gmail.com>
2025-02-07 11:21:54 +0530

    updated files

--- src/train_and_evaluate.py
+++ src/train_and_evaluate.py
@@ -9,8 +9,16 @@ from sklearn.metrics import mean_absolute_error,mean_squared_error, r2_score
 from sklearn.linear_model import ElasticNet
 import joblib
 import json
-import mlflow # type: ignore
+import mlflow
 from urllib.parse import urlparse
+
+
+def eval_metrics(actual, pred):
+    rmse = np.sqrt(mean_squared_error(actual, pred))
+    mae = mean_absolute_error(actual, pred)
+    r2 = r2_score(actual, pred)
+    return rmse, mae, r2
+
 
 def train_and_evaluate(config_path):
     config = read_param(config_path)
@@ -26,8 +34,8 @@ def train_and_evaluate(config_path):
     l1_ratio = config["estimators"]["ElasticNet"]["params"]["l1_ratio"]
 
     target = config["base"]["target_col"]
-    train = pd.read_csv("train_data_path")
-    test = pd.read_csv("test_data_path")
+    train = pd.read_csv(train_data_path)
+    test = pd.read_csv(test_data_path)
 
     train_y = train[target]
     test_y = test[target]
@@ -35,15 +43,42 @@ def train_and_evaluate(config_path):
     train_x = train.drop(target, axis=1)
     test_x = test.drop(target, axis=1)
 
-    lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=random_state)
-    lr.fit(train_x, train_y)
-    
-    predicted_qualities = lr.predict(test_x)
-    (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities) # type: ignore
-    print(f"Elasticnet model (alpha=%f, l1_ratio=%f ):" % (alpha, l1_ratio))
+    ###########################
 
-    if __name__=="__main__":
-        args = argparse.ArgumentParser()
-        args.add_argument("--config", default="params.yml")
-        parsed_args= args.parse_args()
-        train_and_evaluate(config_path=parsed_args.config)
+    lr = ElasticNet(alpha=alpha, l1_ratio= l1_ratio, random_state= random_state)
+    lr.fit(train_x, train_y)
+
+    predicted_qualities = lr.predict(test_x)
+
+    (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
+
+    print("ElasticNet model (alpha=%f, l1_ratio=%f):" % (alpha, l1_ratio))
+
+    score_files = config["reports"]["score"]
+    params_file = config["reports"]["params"]
+
+    with open(score_files, "w") as f:
+        scores = {
+            "rmse": rmse,
+            "mae": mae,
+            "r2": r2
+        }
+        json.dump(scores, f)
+    
+    with open(params_file, "w") as f:
+        params = {
+            "alpha": alpha,
+            "l1_ratio": l1_ratio
+        }
+        json.dump(params, f)
+
+    model_path = config["model_path"]
+    joblib.dump(lr, model_path)
+
+
+
+if __name__=="__main__":
+    args = argparse.ArgumentParser()
+    args.add_argument("--config", default="params.yml")
+    parsed_args= args.parse_args()
+    train_and_evaluate(config_path=parsed_args.config)
